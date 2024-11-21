424aebec9e476d2664ea807de569339e0b6cc4f5
k2works <kakimomokuri@gmail.com>
2024-11-21 15:42:25 +0900

    build:リセット

--- core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/payment/PreparePaymentCommand.java
+++
@@ -1,9 +0,0 @@
-package io.axoniq.demo.bikerental.coreapi.payment;
-
-import org.axonframework.commandhandling.RoutingKey;
-import org.axonframework.modelling.command.TargetAggregateIdentifier;
-
-public record PreparePaymentCommand(int amount, @RoutingKey String paymentReference) {
-}
-
-
--- core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/rental/RegisterBikeCommand.java
+++
@@ -1,9 +0,0 @@
-package io.axoniq.demo.bikerental.coreapi.rental;
-
-import org.axonframework.modelling.command.TargetAggregateIdentifier;
-
-public record RegisterBikeCommand(@TargetAggregateIdentifier String bikeId, // <1>
-                                  String bikeType,
-                                  String location) {
-}
-
